0a0ec5f684d0debe9efecf4b8612ee1ae935be76
Tristan Tinning <tristan_tinning@hotmail.com>
2022-08-01 22:03:38 +0100

    cuisine and sub cuisine enums added

---
+++ src/main/java/com/codeclan/example/mealservice/models/CuisineType.java
@@ -0,0 +1,15 @@
+package com.codeclan.example.mealservice.models;
+
+public enum CuisineType {
+    INDIAN,
+    ITALIAN,
+    THAI,
+    MEXICAN,
+    VIETNAMESE,
+    SPANISH,
+    LEBANESE,
+    JAPANESE,
+    BRITISH,
+    CHINESE,
+    GREEK;
+}
---
+++ src/main/java/com/codeclan/example/mealservice/models/SubCuisineType.java
@@ -0,0 +1,17 @@
+package com.codeclan.example.mealservice.models;
+
+public enum SubCuisineType {
+    PASTA,
+    PIZZA,
+    NOODLES,
+    CURRY,
+    FAJITAS,
+    BURRITO,
+    ENCHALADAS,
+    GYROS,
+    STIRFRY,
+    TAPAS,
+    RAMAN,
+    SCOTTISH,
+    ENGLISH;
+}
