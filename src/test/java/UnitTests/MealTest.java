e35d487e8972258556168fa6850954b168da0d8f
Tristan Tinning <tristan_tinning@hotmail.com>
2022-08-01 21:58:35 +0100

    tests added

--- src/test/java/UnitTests/MealTest.java
+++ src/test/java/UnitTests/MealTest.java
@@ -1,4 +1,30 @@
 package UnitTests;
 
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class MealTest {
+
+    Meal meal;
+    @Before
+    public void before() {
+        meal = new Meal("Crab Linguine", CuisineType.ITALIAN, SubCuisineType.PASTA);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Crab Linguine", meal.getName());
+    }
+
+    @Test
+    public void hasCuisineType() {
+        assertEquals(CuisineType.ITALIAN, meal.getCuisineType());
+    }
+
+    @Test
+    public void hasSubCuisineType() {
+        assertEquals(SubCuisineType.PASTA, meal.getSubCuisineType());
+    }
 }
