fb9905e4ee5bf49fc629baa2b0c5c2a8542f098f
Tristan Tinning <tristan_tinning@hotmail.com>
2022-08-01 20:57:22 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/mealservice/MealserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.mealservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealserviceApplication.class, args);
+	}
+
+}
